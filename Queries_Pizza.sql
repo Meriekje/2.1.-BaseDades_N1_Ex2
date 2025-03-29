2617e33552a2a7f62683269a1158c1ae622de6e0
Meriekje <meritxellara@gmail.com>
2025-03-29 08:56:41 +0100

    Update Queries_Pizza.sql

--- Queries_Pizza.sql
+++ Queries_Pizza.sql
@@ -1,11 +1,12 @@
 - Drinks sold - Girona
 SELECT 
-    e.first_name AS employee_name, 
-    COUNT(o.id_order) AS total_orders
-FROM orders o
-JOIN employees e USING (id_employee)
-WHERE e.first_name = 'Laura'  
-GROUP BY e.first_name;
+	COUNT(po.quantity) AS total_drinks_sold
+FROM product_orders po
+JOIN products p USING(id_product)
+JOIN orders o USING(id_order)
+JOIN shops s USING(id_shop)
+JOIN locations l USING(id_town)
+WHERE p.type = 'drink' AND l.name= 'Girona' ;
 
 - Orders via Laura
 SELECT 
